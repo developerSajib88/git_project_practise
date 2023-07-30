c001dfb6f3a76de3ed57970750d49bba5b557f88
developerSajib88 <developersajib88@gmail.com>
2023-07-30 09:46:53 +0600

    third brance commit

---
+++ lib/second_class.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SecondClass extends StatefulWidget {
+  const SecondClass({super.key});
+
+  @override
+  State<SecondClass> createState() => _SecondClassState();
+}
+
+class _SecondClassState extends State<SecondClass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
