7367b4fdacb97d154c125339f23402416188d88c
developerSajib88 <developersajib88@gmail.com>
2023-07-30 09:55:51 +0600

    Last branch commit

---
+++ lib/third_class.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThirdClass extends StatefulWidget {
+  const ThirdClass({super.key});
+
+  @override
+  State<ThirdClass> createState() => _ThirdClassState();
+}
+
+class _ThirdClassState extends State<ThirdClass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
