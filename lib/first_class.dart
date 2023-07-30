f70ecfacd91c7aebcd5abcb3353c059925aafaac
developerSajib88 <developersajib88@gmail.com>
2023-07-30 09:43:14 +0600

    second brance commit

---
+++ lib/first_class.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirstClass extends StatefulWidget {
+  const FirstClass({super.key});
+
+  @override
+  State<FirstClass> createState() => _FirstClassState();
+}
+
+class _FirstClassState extends State<FirstClass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
